61f605e2b77321e4d1525cbc59bb7ab29ed41af6
Manojlovic98 <manojlovic911@hotmail.com>
2021-02-20 06:50:47 +0100

    Add port range for the target machine

--- portScanner.py
+++ portScanner.py
@@ -2,15 +2,19 @@ import socket
 
 from IPy import IP
 
+def scan_port(ipAdress, port):
+    try:
+        # Defines socket descriptor.
+        sock = socket.socket()
+        # timeout
+        sock.settimeout(0.5)
+        # Connect to target machine.
+        sock.connect((ipAdress, port))
+        print(f"[+] Port {str(port)} is open!")
+    except:
+        print(f"[-] Port {str(port)} is closed.")
 
 ipAdress = input('[+] Enter Target To Scan: ')
-port = 80
 
-try:
-    """Defines socket descriptor."""
-    sock = socket.socket()
-    """Connect to target machine."""
-    sock.connect((ipAdress, port))
-    print("[+] Port 80 is open!")
-except:
-    print("[-] Port 80 is closed.")
+for port in range(75, 85):
+    scan_port(ipAdress, port)
