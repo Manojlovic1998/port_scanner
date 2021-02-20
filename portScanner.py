af16a6cdb39e9220c19d608e0a6178ced4b27cf9
Manojlovic98 <manojlovic911@hotmail.com>
2021-02-20 07:39:40 +0100

    Add function to check address, check_ip

--- portScanner.py
+++ portScanner.py
@@ -2,7 +2,35 @@ import socket
 
 from IPy import IP
 
+
+def check_ip(address):
+    """Checks the ipAddress value, if it is
+    ip address of the target then it returns it.
+    If the value is domain name then it converts it
+    to the ip address.
+
+    Args:
+        address (string): It can be an Ip address or it
+        can be a domain name.
+
+    Returns:
+        [string]: Returns ip address of targeted machine.
+    """
+    try:
+        IP(address)
+        return address
+    except ValueError:
+        return socket.gethostbyname(address)
+
+
 def scan_port(ipAdress, port):
+    """Makes connection with target machine using ipAdress
+    and then it scans for open ports.
+    
+    Args:
+        ipAdress (string): Ip address of the machine you are targeting. 
+        port (integer): Number of port to be checked.
+    """
     try:
         # Defines socket descriptor.
         sock = socket.socket()
@@ -15,6 +43,7 @@ def scan_port(ipAdress, port):
         print(f"[-] Port {str(port)} is closed.")
 
 ipAdress = input('[+] Enter Target To Scan: ')
+convert_address = check_ip(ipAdress)
 
 for port in range(75, 85):
     scan_port(ipAdress, port)
